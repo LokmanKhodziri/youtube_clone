9b3b21229f0d23f88b4414a9359c49202831ef98
lokmankhodziri <lokmankhodziri@gmail.com>
2023-11-22 23:13:50 +0800

    Changing to src folder

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Feed } from '@mui/icons-material';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      Navbar
+      <Routes>
+        <Route path='/' exact element={<Feed />}></Route>
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+
+export default App
